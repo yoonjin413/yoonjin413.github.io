8080f65c2c1b73b89c0b7d9393fd3772ecf28aea
yoonjin413 <yoonjin413@naver.com>
2020-12-05 19:38:01 +0900

    도로교통공사 :: sect01

--- koroad/js/koroad.js
+++ koroad/js/koroad.js
@@ -16,6 +16,26 @@ $(document).ready(function(){
         slideWidth: 950,
         pagerCustom: '.sub06_slide .tab'
     });
+
+    /* .slide_area slider */
+    $('.slide_area ul').bxSlider({
+        auto: true,
+        pager: false,
+        controls: false,
+        infiniteLoop:true,
+        slideWidth:995,
+        //slideMargin:10,
+        onSliderLoad:function(currentIndex){
+            $('.slide_area ul li').eq(currentIndex+1).addClass('on');
+        },
+        onSlideBefore:function($slideElement, oldIndex, newIndex){
+            $('.slide_area ul li').removeClass('on');
+            $($slideElement).addClass('on');
+        }
+    });
+
+
+
 });
 
 // 해당 영역으로 이동하기
