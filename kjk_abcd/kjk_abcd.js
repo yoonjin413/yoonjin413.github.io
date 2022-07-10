1577f1d65becd520ab7936f90e2efe90623a8d90
yoonjin413 <yoonjin413@naver.com>
2022-07-10 16:00:55 +0900

    [kjk_abcd] 작업 중 :: sect03

--- kjk_abcd/kjk_abcd.js
+++ kjk_abcd/kjk_abcd.js
@@ -2,4 +2,14 @@ $(function() {
     setTimeout(function() {
         console.log('this is kjk_abcd.html');
     }, 100);
+
+    $('.cont_slide ul').bxSlider({
+        pager: false,
+        infiniteLoop:true,
+        slideWidth: 590,
+        slideMargin:24,
+        speed: 30000,
+        ticker: true
+    });
 });
+
